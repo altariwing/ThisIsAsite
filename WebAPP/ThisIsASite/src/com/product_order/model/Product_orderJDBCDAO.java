503ffc7fc408154b9252cad2ed256ae1750a77dd
altariwing <altariwing@gmail.com>
2017-11-08 16:59:47 +0800

    UPD
    UPD

--- WebAPP/ThisIsASite/src/com/product_order/model/Product_orderJDBCDAO.java
+++ WebAPP/ThisIsASite/src/com/product_order/model/Product_orderJDBCDAO.java
@@ -3,7 +3,15 @@ package com.product_order.model;
 import java.util.List;
 
 public class Product_orderJDBCDAO implements Product_orderDAO_interface{
-
+	String driver = "oracle.jdbc.driver.OracleDriver";
+	String url = "jdbc:oracle:thin:@localhost:1521:XE";
+	String userid = "BA104";
+	String passwd = "123456";
+	private static final String INSERT =
+			"INSERT INTO PRODUCT_ORDER(PDO_NO, MEM_NO, SLR_NO)"
+			+"VALUES('OD'||TO_CHAR(SYSDATE,'RRMMDD')||'-'||(LPAD(TO_CHAR(PDO_NO_SEQ.NEXTVAL),6,'0')), ?, ?)";
+	
+	
 	@Override
 	public void insert(Product_orderVO Product_orderVO) {
 		// TODO Auto-generated method stub
